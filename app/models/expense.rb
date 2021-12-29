4d588db255af04a3c631b22f8d28e0e53a79962c
Juan Carlos Meza <juancmezat@gmail.com>
2021-12-29 02:01:17 -0800

    includes validations for expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :amount, :date, :description, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    has_many :expenses
+    has_many :expenses, :dependent => :destroy
     has_many :categories, through: :expenses
-  
+
     def self.grouped_expenses(user_id)
       user = User.where(id: user_id)[0]
       user_expenses = user.expenses
       grouped_categories = []
       uniq_categories = user_expenses.map { |expense| expense[:category_id] }.uniq
-  
+
       uniq_categories.each do |category|
         user_expenses.each do |expense|
           if expense[:category_id] == category
@@ -28,11 +28,9 @@ class User < ApplicationRecord
       total_amount = grouped_categories.map{|category| category[:amount]}.sum
       {categories: grouped_categories, totalAmount: total_amount}
     end
-  
+
     def getCategoryName(id)
       self.categories.find(id).name
     end
-  
+
   end
-  
-  
