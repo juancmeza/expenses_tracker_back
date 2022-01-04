fdf9fe40b433462339521983a0a3d90e4c0b53e7
Juan Carlos Meza <juancmezat@gmail.com>
2022-01-04 03:57:23 -0800

    Personalized messages for validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,11 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :amount, :date, :description, :category_id, presence: true
+  validates :amount, presence: true, :message => "Be sure to include the total"
+  validates :date, presence: true, :message => "Be sure to include a date"
+  validates :description, presence: true, :message => "Be sure to include a description"
+  validates :category_id, presence: true, :message => "Be sure to select a category"
+
+  validates :amount, type => Number, :message => "The amount must be a number"
 
 end
