f8a95f259ee5d48aeba5ba67d26211c5e7fe64fd
Juan Carlos Meza <juancmezat@gmail.com>
2022-01-12 04:36:46 -0800

    Temporary rollback to validations without custom error messages

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,11 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :amount, presence: true, :message => "Be sure to include the total"
-  validates :date, presence: true, :message => "Be sure to include a date"
-  validates :description, presence: true, :message => "Be sure to include a description"
-  validates :category_id, presence: true, :message => "Be sure to select a category"
-
-  validates :amount, type => Number, :message => "The amount must be a number"
+  validates :amount, :date, :description, :category_id, presence: true
 
 end
