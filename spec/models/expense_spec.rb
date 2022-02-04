dc0bc58f9ee54ac93cd0b42cff3c903262d083d2
Juan Carlos Meza <juancmezat@gmail.com>
2022-02-03 19:20:28 -0800

    Expense Model testing - Changes actually saved

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -8,23 +8,27 @@ RSpec.describe Expense, type: :model do
     test_user = User.new(name: 'TestUser')
 
     it 'is valid with all valid attributes present' do
-      expect(Expense.new(amount: 10.0, date: Date.today, description: 'TestExp', category_id: 1))
+      expect(Expense.new(amount: 10.0, date: Date.parse('2022-1-1'), description: 'TestExp', category_id: 1, user_id: ${test_user.id})).to be_valid
     end
 
     it 'is not valid without an amount' do
-
+      expect(Expense.new(date: Date.parse('2022-1-1'), description: 'TestExp', category_id: 1, user_id: ${test_user.id})).to_not be_valid
     end
 
     it 'is not valid without a date' do
-
+      expect(Expense.new(amount: 10.0, description: 'TestExp', category_id: 1, user_id: ${test_user.id})).to_not be_valid
     end
 
     it 'is not valid without a description' do
-
+      expect(Expense.new(amount: 10.0, date: Date.parse('2022-1-1'), category_id: 1, user_id: ${test_user.id})).to_not be_valid
     end
 
     it 'is not valid without a category id' do
+      expect(Expense.new(amount: 10.0, date: Date.parse('2022-1-1'), description: 'TestExp', user_id: ${test_user.id})).to_not be_valid
+    end
 
+    it 'is not valid without a user id' do
+      expect(Expense.new(amount: 10.0, date: Date.parse('2022-1-1'), description: 'TestExp', category_id: 1)).to be_valid
     end
   end
 end
